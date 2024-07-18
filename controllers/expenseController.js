1ac1a10748f0c2513ae621f0876befbdc629bbc9
Andrew Acin <andrew_acin@hotmail.com>
2024-07-17 18:16:23 -0700

    fixed node mod and dependencies and controller

--- controllers/expenseController.js
+++ controllers/expenseController.js
@@ -13,11 +13,28 @@ expenses.get('/', async (req, res) => {
     }
 });
 
+// Find specific expenses
+expenses.get('/:id', async (req, res) => {
+    try {
+        const foundExpenses = await Expense.findOne({
+            where: { id:req.params.id}
+        });
+        if(foundExpenses) {
+            res.status(200).json(foundExpenses)
+        } else {
+            res.status(404).json({message: 'Expense not found'})
+        }
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+
+
 // create an expense
 expenses.post('/', async (req, res) => {
     try {
         const newExpense = await Expense.create(req.body)
-        res.status(200).json({
+        res.status(201).json({
             message: 'Successfully inserted an expense',
             data: newExpense
         })
